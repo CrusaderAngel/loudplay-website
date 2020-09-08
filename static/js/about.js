b552f5604cef32ce0ce6ffb98a4581786a8e8695
Mr. Dudin <igor.dudin.2013@mail.ru>
2020-09-08 22:45:48 +0600

    fix: minor navbar fixes

--- static/js/about.js
+++ static/js/about.js
@@ -1,32 +1,31 @@
 if (window.screen.width > 421) {
-    var mySwiper = new Swiper('.swiper-container', {
-      slidesPerView: 3.26,
-  
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-  
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
-        snapOnRelease: false,
-      },
-    })
-  } else {
-    var mySwiper = new Swiper('.swiper-container', {
-      slidesPerView: 1.45,
-  
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-  
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
-        snapOnRelease: false,
-      },
-    })
-  }
-  
+  var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+      snapOnRelease: false,
+    },
+  })
+} else {
+  var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1.45,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+      snapOnRelease: false,
+    },
+  })
+}
