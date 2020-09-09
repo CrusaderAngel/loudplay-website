66345b014dcb3fc08d72601a5c37ae30c697749a
DKlygov <dklygov@gmail.com>
2020-09-09 18:50:58 +0600

    feat: add roistat

--- static/js/index.js
+++ static/js/index.js
@@ -5,13 +5,16 @@ import BurgerManipulator from './BurgerManipulator';
 window.addEventListener('DOMContentLoaded', LanguageManipulator.setDefaultLanguage);
 window.addEventListener('DOMContentLoaded', renderDefaultText);
 document.querySelectorAll('#globe').forEach(item => item.addEventListener('click', switchLang));
-window.addEventListener('DOMContentLoaded', initGTM)
+window.addEventListener('load', getRoistatId);
+window.addEventListener('DOMContentLoaded', initGTM);
+
 
 
 
 const CURRENTLANGUAGE = LanguageManipulator.getCurrentLanguage();
 const PAGENAME = LanguageManipulator.getPagename();
 const ELEMENTS = LanguageManipulator.getElementsToTranslate(PAGENAME);
+
 
 if (CURRENTLANGUAGE == 'en' && PAGENAME != 'download') {
     document.querySelector('#globe').src = '/assets/misc/globe-en.svg'
@@ -43,7 +46,7 @@ function initGTM() {
 
 function renderDefaultText() {
     LanguageManipulator.translateTo(CURRENTLANGUAGE, PAGENAME, ELEMENTS);
-    if(CURRENTLANGUAGE == 'en' && screen.width > screen.height && PAGENAME == '/') {
+    if (CURRENTLANGUAGE == 'en' && screen.width > screen.height && PAGENAME == '/') {
         document.getElementById('download-link-content').style.padding = '1.2em 5.3em';
     };
 };
@@ -53,13 +56,13 @@ function switchLang() {
     if (NEWLANG == 'en') {
         document.querySelectorAll('#globe').forEach(item => item.src = '/assets/misc/globe-en.svg')
         document.querySelector('#navbar-download-link-mobile').style = 'margin-left: 0.5em';
-        if(screen.width > screen.height && PAGENAME == '/') {
+        if (screen.width > screen.height && PAGENAME == '/') {
             document.getElementById('download-link-content').style.padding = '1.2em 5.3em';
         };
     } else {
         document.querySelectorAll('#globe').forEach(item => item.src = '/assets/misc/globe.svg')
         document.querySelector('#navbar-download-link-mobile').style = 'margin-left: 0em'
-        if(screen.width < screen.height && PAGENAME == '/') {
+        if (screen.width < screen.height && PAGENAME == '/') {
             document.getElementById('download-link-content').style.padding = '1.2em 4.6em !important';
         };
     }
@@ -100,3 +103,43 @@ if (PAGENAME != 'download') {
 function redirectBack() {
     window.history.back();
 }
+
+function getRoistatId() {
+
+    let roistatVisit = document.cookie.slice(
+      document.cookie.indexOf('roistat_visit=') + 14,
+      document.cookie.indexOf(
+        ';',
+        document.cookie.indexOf('roistat_visit=') + 14
+      )
+    )
+  
+    if (roistatVisit == 'null') roistatVisit = '';
+    return roistatVisit
+  }
+  
+  document.querySelectorAll('.download_win').forEach(function (el) {
+    el.onclick = function () {
+  
+      window.location.replace(`https://dl.loudplay.ru/l/loudplay/win/loudplay-latest.exe${getRoistatId() ? '?id=' + getRoistatId() : ''}`);
+      dataLayer.push({ event: 'Download_Launcher' })
+      return false;
+    };
+  });
+  
+  document.querySelectorAll('.download_mac').forEach(function (el) {
+    el.onclick = function () {
+  
+      window.location.replace(`https://dl.loudplay.ru/l/loudplay/mac/loudplay-latest.dmg${getRoistatId() ? '?id=' + getRoistatId() : ''}`);
+      dataLayer.push({ event: 'Download_Launcher' })
+      return false;
+    };
+  });
+  
+  document.querySelectorAll('.download_android').forEach(function (el) {
+    el.onclick = function () {
+      window.location.replace(`https://dl.loudplay.ru/l/loudplay/android/apk/loudplay.apk${getRoistatId() ? '?id=' + getRoistatId() : ''}`);
+      dataLayer.push({ event: 'Download_Launcher' })
+      return false;
+    };
+  });
